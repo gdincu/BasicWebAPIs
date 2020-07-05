113d623dd73233c428aca04d5f89329b1b3eebcb
gdincu <dici0137@scs.ubbcluj.ro>
2020-07-05 20:41:59 +0300

    Basic CRUD Razor Page

---
+++ Data/BasicWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BasicWebAPI.Pages.Models;
+
+namespace BasicWebAPI.Data
+{
+    public class BasicWebAPIContext : DbContext
+    {
+        public BasicWebAPIContext (DbContextOptions<BasicWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BasicWebAPI.Pages.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BasicWebAPI.Data;
+using BasicWebAPI.Pages.Models;
+
+namespace BasicWebAPI.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly BasicWebAPI.Data.BasicWebAPIContext _context;
+
+        public DetailsModel(BasicWebAPI.Data.BasicWebAPIContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
